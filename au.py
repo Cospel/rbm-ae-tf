4c5b4c1aaedad11ff9499d2152e8057e1289177e
MichalLukac <cospelthetraceur@gmail.com>
2016-04-12 00:57:19 +0200

    update

--- au.py
+++ au.py
@@ -3,8 +3,9 @@ from utilsnn import xavier_init
 
 
 class AutoEncoder(object):
-    def __init__(self, input_size, layer_sizes, layer_names, optimizer=tf.train.AdamOptimizer(),
+    def __init__(self, input_size, layer_sizes, layer_names, tied_weights=False, optimizer=tf.train.AdamOptimizer(),
                  transfer_function=tf.nn.sigmoid):
+        self.tied_weights = tied_weights
         # Build the encoding layers
         self.x = tf.placeholder("float", [None, input_size])
         next_layer_input = self.x
@@ -18,7 +19,7 @@ class AutoEncoder(object):
             input_dim = int(next_layer_input.get_shape()[1])
 
             # Initialize W using xavier initialization]
-            W = tf.Variable(xavier_init(input_dim, dim, transfer_function), name=layer_names[i][0])
+            W = tf.Variable(tf.zeros([input_dim, dim]), name=layer_names[i][0])
 
             # Initialize b to zero
             b = tf.Variable(tf.zeros([dim]), name=layer_names[i][1])
@@ -39,15 +40,28 @@ class AutoEncoder(object):
         layer_sizes.reverse()
         self.encoding_matrices.reverse()
 
+        self.decoding_matrices = []
+        self.decoding_biases = []
+
         for i, dim in enumerate(layer_sizes[1:] + [int(self.x.get_shape()[1])]):
-            # we are using tied weights, so just lookup the encoding matrix for this step and transpose it
-            W = tf.transpose(self.encoding_matrices[i])
+            print('test')
+            print(self.encoding_matrices[i].get_shape())
+            W = None
+            # if we are using tied weights, so just lookup the encoding matrix for this step and transpose it
+            if tied_weights:
+                W = tf.identity(tf.transpose(self.encoding_matrices[i]))
+            else:
+                W = tf.Variable(tf.zeros([self.encoding_matrices[i].get_shape()[1],self.encoding_matrices[i].get_shape()[0]]))
             b = tf.Variable(tf.zeros([dim]))
+            self.decoding_matrices.append(W)
+            self.decoding_biases.append(b)
+
             output = transfer_function(tf.matmul(next_layer_input, W) + b)
             next_layer_input = output
 
         # i need to reverse the encoding matrices back for loading weights
         self.encoding_matrices.reverse()
+        self.decoding_matrices.reverse()
 
         # the fully encoded and reconstructed value of x is here:
         self.reconstructed_x = next_layer_input
@@ -67,10 +81,23 @@ class AutoEncoder(object):
     def reconstruct(self, X):
         return self.sess.run(self.reconstructed_x, feed_dict={self.x: X})
 
-    def restore_weights(self, path, layer_names, layer):
+    def restore_tied_weights(self, path, layer_names, layer):
         saver = tf.train.Saver({layer_names[0]: self.encoding_matrices[layer]},
                                {layer_names[1]: self.encoding_biases[layer]})
         saver.restore(self.sess, path)
+
+        if not self.tied_weights:
+            self.sess.run(self.decoding_matrices[layer].assign(tf.transpose(self.encoding_matrices[layer])))
+
+    def print_weights(self):
+        print('encoding matrices')
+        for i in range(len(self.encoding_matrices)):
+            print(i)
+            print(self.encoding_matrices[i].eval(self.sess).shape)
+            print(self.encoding_matrices[i].eval(self.sess))
+            if not self.tied_weights:
+                print(self.decoding_matrices[i].eval(self.sess).shape)
+                print(self.decoding_matrices[i].eval(self.sess))
 
     def save_weights(self, path):
         pass
