a5e333fbb7fa9724cf2424734962f495df71de8e
MichalLukac <cospelthetraceur@gmail.com>
2016-04-12 12:39:37 -0400

    update

---
+++ test-ae-rbm.py
@@ -0,0 +1,42 @@
+from rbm import RBM
+from au import AutoEncoder
+import tensorflow as tf
+import input_data
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
+
+# First RBM
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+rbmobject1 = RBM(784, 100, ['rbmw1', 'rbvb1', 'rbmhb1'], 0.001)
+
+# Second RBM
+rbmobject2 = RBM(100, 20, ['rbmw2', 'rbvb2', 'rbmhb2'], 0.001)
+
+# Autoencoder
+autoencoder = AutoEncoder(784, [100, 20],  [['rbmw1', 'rbmhb1'],
+                                            ['rbmw2', 'rbmhb2']])
+
+# Train First RBM
+for i in range(10000):
+  batch_xs, batch_ys = mnist.train.next_batch(10)
+  cost = rbmobject1.partial_fit(batch_xs)
+rbmobject1.save_weights('./rbmw1.chp')
+
+# Train Second RBM
+for i in range(10000):
+  # Transform features with first rbm for second rbm
+  batch_xs, batch_ys = mnist.train.next_batch(10)
+  batch_xs = rbmobject1.transform(batch_xs)
+  cost = rbmobject2.partial_fit(batch_xs)
+rbmobject2.save_weights('./rbmw2.chp')
+
+# Load RBM weights to Autoencoder
+autoencoder.load_weights('./rbmw1.chp', ['rbmw1', 'rbmhb1'], 0)
+autoencoder.load_weights('./rbmw2.chp', ['rbmw2', 'rbmhb2'], 1)
+
+# Train Autoencoder
+for i in range(5000):
+  batch_xs, batch_ys = mnist.train.next_batch(10)
+  cost = autoencoder.partial_fit(batch_xs)
