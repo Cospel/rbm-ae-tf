cecbcabb181841f68c44cd266e70760b7cadd158
cospel <MichalLukac>
2016-04-14 00:19:18 +0200

    update

--- rbm.py
+++ rbm.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 
 class RBM(object):
@@ -11,26 +12,40 @@ class RBM(object):
         network_weights = self._initialize_weights()
         self.weights = network_weights
 
-        # model
+        # placeholders
         self.x = tf.placeholder(tf.float32, [None, self.n_input])
-        self.h0 = self.sample_prob(transfer_function(tf.matmul(self.x, self.weights['w']) + self.weights['hb']))
+        self.rbm_w = tf.placeholder(tf.float32, [self.n_input, self.n_hidden])
+        self.rbm_vb = tf.placeholder(tf.float32, [self.n_input])
+        self.rbm_hb = tf.placeholder(tf.float32, [self.n_hidden])
+
+        # variables
+        self.n_w = np.zeros([self.n_input, self.n_hidden], np.float32)
+        self.n_vb = np.zeros([self.n_input], np.float32)
+        self.n_hb = np.zeros([self.n_hidden], np.float32)
+        self.o_w = np.zeros([self.n_input, self.n_hidden], np.float32)
+        self.o_vb = np.zeros([self.n_input], np.float32)
+        self.o_hb = np.zeros([self.n_hidden], np.float32)
+
+        # models
+        self.h0 = self.sample_prob(transfer_function(tf.matmul(self.x, self.rbm_w) + self.rbm_hb))
         self.v1 = self.sample_prob(
-            transfer_function(tf.matmul(self.h0, tf.transpose(self.weights['w'])) + self.weights['vb']))
-        self.h1 = tf.nn.sigmoid(tf.matmul(self.v1, self.weights['w']) + self.weights['hb'])
+            transfer_function(tf.matmul(self.h0, tf.transpose(self.rbm_w)) + self.rbm_vb))
+        self.h1 = tf.nn.sigmoid(tf.matmul(self.v1, self.rbm_w) + self.rbm_hb)
 
         # compute gradients
         self.w_positive_grad = tf.matmul(tf.transpose(self.x), self.h0)
         self.w_negative_grad = tf.matmul(tf.transpose(self.v1), self.h1)
 
         # compute updates and add them to weights
-        self.update_w = self.weights['w'].assign_add(alpha * (self.w_positive_grad - self.w_negative_grad))
-        self.update_vb = self.weights['vb'].assign_add(alpha * tf.reduce_mean(self.x - self.v1, 0))
-        self.update_hb = self.weights['hb'].assign_add(alpha * tf.reduce_mean(self.h0 - self.h1, 0))
+        self.update_w = self.rbm_w + alpha * (self.w_positive_grad - self.w_negative_grad) / tf.to_float(
+            tf.shape(self.x)[0])
+        self.update_vb = self.rbm_vb + alpha * tf.reduce_mean(self.x - self.v1, 0)
+        self.update_hb = self.rbm_hb + alpha * tf.reduce_mean(self.h0 - self.h1, 0)
 
         # sampling
-        self.h_sample = self.sample_prob(transfer_function(tf.matmul(self.x, self.weights['w']) + self.weights['hb']))
+        self.h_sample = self.sample_prob(transfer_function(tf.matmul(self.x, self.rbm_w) + self.rbm_hb))
         self.v_sample = self.sample_prob(
-            transfer_function(tf.matmul(self.h_sample, tf.transpose(self.weights['w'])) + self.weights['vb']))
+            transfer_function(tf.matmul(self.h_sample, tf.transpose(self.rbm_w)) + self.rbm_vb))
 
         # cost
         self.err_sum = tf.reduce_mean(tf.square(self.x - self.v_sample))
@@ -38,6 +53,10 @@ class RBM(object):
         init = tf.initialize_all_variables()
         self.sess = tf.Session()
         self.sess.run(init)
+
+    def compute_cost(self, batch):
+        return self.sess.run(self.err_sum, feed_dict={self.x: batch, self.rbm_w: self.o_w,
+                                                      self.rbm_vb: self.o_vb, self.rbm_hb: self.o_hb})
 
     def sample_prob(self, probs):
         return tf.nn.relu(tf.sign(probs - tf.random_uniform(tf.shape(probs))))
@@ -50,14 +69,21 @@ class RBM(object):
         all_weights['hb'] = tf.Variable(tf.random_uniform([self.n_hidden], dtype=tf.float32), name=self.layer_names[2])
         return all_weights
 
-    def transform(self, X):
-        return self.sess.run(self.h1, {self.x: X})
+    def transform(self, batch_x):
+        return self.sess.run(self.h_sample, {self.x: batch_x, self.rbm_w: self.o_w,
+                                             self.rbm_vb: self.o_vb, self.rbm_hb: self.o_hb})
 
     def restore_weights(self, path):
         saver = tf.train.Saver({self.layer_names[0]: self.weights['w'],
                                 self.layer_names[1]: self.weights['vb'],
                                 self.layer_names[2]: self.weights['hb']})
+
         saver.restore(self.sess, path)
+
+        self.o_w = self.weights['w'].eval(self.sess)
+        self.o_vb = self.weights['vb'].eval(self.sess)
+        self.o_hb = self.weights['hb'].eval(self.sess)
+
 
     def save_weights(self, path):
         saver = tf.train.Saver({self.layer_names[0]: self.weights['w'],
@@ -68,12 +94,20 @@ class RBM(object):
     def return_weights(self):
         return self.weights
 
-    def return_hidden_weight_as_np(self, name):
-        return self.weights[name].eval(self.sess)
+    def return_hidden_weight_as_np(self):
+        return self.n_w
 
     def partial_fit(self, batch_x):
-        self.sess.run([self.update_w,
-                       self.update_vb,
-                       self.update_hb], {self.x: batch_x})
+        self.n_w, self.n_vb, self.n_hb = self.sess.run([self.update_w, self.update_vb, self.update_hb],
+                                                       feed_dict={self.x: batch_x, self.rbm_w: self.o_w,
+                                                                  self.rbm_vb: self.o_vb, self.rbm_hb: self.o_hb})
 
-        return self.sess.run(self.err_sum, feed_dict={self.x: batch_x})
+        self.o_w = self.n_w
+        self.o_vb = self.n_vb
+        self.o_hb = self.n_hb
+
+        self.sess.run(self.weights['w'].assign(self.o_w))
+        self.sess.run(self.weights['vb'].assign(self.o_vb))
+        self.sess.run(self.weights['hb'].assign(self.o_hb))
+        return self.sess.run(self.err_sum, feed_dict={self.x: batch_x, self.rbm_w: self.n_w, self.rbm_vb: self.n_vb,
+                                                      self.rbm_hb: self.n_hb})
--- test-ae-rbm.py
+++ test-ae-rbm.py
@@ -9,6 +9,8 @@ flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
 
 # First RBM
 mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
 rbmobject1 = RBM(784, 100, ['rbmw1', 'rbvb1', 'rbmhb1'], 0.001)
 
 # Second RBM
@@ -20,17 +22,23 @@ autoencoder = AutoEncoder(784, [100, 20],  [['rbmw1', 'rbmhb1'],
                                            tied_weights=False)
 
 # Train First RBM
-for i in range(100):
-  batch_xs, batch_ys = mnist.train.next_batch(10)
-  cost = rbmobject1.partial_fit(batch_xs)
+epoch = 2
+for i in range(epoch):
+  for i in range(100):
+    print(i)
+    batch_xs, batch_ys = mnist.train.next_batch(10)
+    cost = rbmobject1.partial_fit(batch_xs)
+  print(rbmobject1.compute_cost(trX))
 rbmobject1.save_weights('./rbmw1.chp')
 
 # Train Second RBM
 for i in range(100):
+  print(i)
   # Transform features with first rbm for second rbm
   batch_xs, batch_ys = mnist.train.next_batch(10)
   batch_xs = rbmobject1.transform(batch_xs)
   cost = rbmobject2.partial_fit(batch_xs)
+print(rbmobject2.compute_cost(rbmobject1.transform(trX)))
 rbmobject2.save_weights('./rbmw2.chp')
 
 # Load RBM weights to Autoencoder
